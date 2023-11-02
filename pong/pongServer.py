fd01f4d257002dbf6c68741c6c67f6345ffd5928
gek <gek>
2023-11-01 22:01:40 -0400

    gitap

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -1,9 +1,8 @@
 # =================================================================================================
-# Contributing Authors:	    <Anyone who touched the code>
-# Email Addresses:          <Your uky.edu email addresses>
-# Date:                     <The date the file was last edited>
-# Purpose:                  <How this file contributes to the project>
-# Misc:                     <Not Required.  Anything else you might want to include>
+# Contributing Authors:	    David MHS Webster
+# Email Addresses:          dwe245@uky.edu
+# Date:                     NYF
+# Purpose:                  it's the server...
 # =================================================================================================
 
 import socket
@@ -15,3 +14,16 @@ import threading
 # for each player and where the ball is, and relay that to each client
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
 # clients are and take actions to resync the games
+
+
+# TODO: implement pong simulation serverside, send updates clientside. Get paddle positions
+# from clients. Don't care if they cheat. 
+
+
+def startServer(port:str, errorLabel:tk.Label, app:tk.Tk) -> None:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+
+
